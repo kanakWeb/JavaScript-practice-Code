869c9423e3d6b37eda83764ce6c9b01051909a4b
Kanak Sarker <kanakwebdev@gmail.com>
2023-12-18 15:24:33 +0600

    practice code added

---
+++ arry.js
@@ -0,0 +1,17 @@
+var student = ['karim', 'rahim', 'siplu', 'siku']
+
+var length = typeof student;
+console.log(student)
+
+console.log(length) //array type check 
+
+console.log(student.length)//length check //output
+
+var positionOfArray = student.indexOf('siplu') //position check
+console.log(positionOfArray) //output
+
+student[1]='kanak'//set new index value
+student.push('roma') //set last new value
+student.pop() //array to out value
+
+console.log(student)//output
---
+++ tofixed.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 45;
+var c = parseFloat(a/ b);
+console.log(c.toFixed(3))
+
+// console.log(5>5)
+// console.log(5!=6)
+// console.log('rahim'==='karim')
+// console.log(5==5)
+// console.log('mylove'=='mylove')
